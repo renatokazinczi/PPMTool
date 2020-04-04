fd8596d9a6ba2a807b531edef19f921bf9121523
erhudec <er.hudec@gmail.com>
2020-04-04 15:47:04 +0200

    First commit

---
+++ PPMToolFullStack/src/test/java/com/renato/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
